7fb81f037e13ea219a05eb97af0faa1163cf351a
Benjamin Likita <benjaminlikita3@gmail.com>
2022-11-28 16:53:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let form = document.getElementById("task_list")
+let list = document.getElementById("list")
+let filter = document.getElementById("filter")
+let clearBtn = document.getElementById("clear")
+let newTask = document.getElementById("newTask")
+let submitBtn = document.getElementById("submit-btn")
+
+submitBtn.addEventListener("click", addTask) 
+list.addEventListener("click", deleteTask)
+
+function addTask(e){
+    if(newTask.value === ""){
+        alert("Add a Task")
+    } else{
+        let li = document.createElement("li")
+        li.innerText = `${newTask.value} `
+        let a = document.createElement("a")
+        a.setAttribute("href", "#")
+        a.innerText = "X"
+        li.appendChild(a)
+        list.appendChild(li)       
+    }
+    e.preventDefault()
+}
+function deleteTask(e){
+    if(e.target.hasAttribute("href")){
+        if(confirm("Are you sure?")){
+            let ele = e.target.parentElement
+            ele.remove()
+        }
+    }
+}
