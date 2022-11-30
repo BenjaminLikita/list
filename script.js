18f54266a07cc4b716695ad8166e02819ede5b1c
Benjamin Likita <benjaminlikita3@gmail.com>
2022-11-30 11:33:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ let submitBtn = document.getElementById("submit-btn")
 
 submitBtn.addEventListener("click", addTask) 
 list.addEventListener("click", deleteTask)
+clearBtn.addEventListener("dblclick", clearTask)
+filter.addEventListener("keyup", filterTask)
 
-function addTask(e){
+function addTask(){
     if(newTask.value === ""){
         alert("Add a Task")
     } else{
@@ -17,9 +19,19 @@ function addTask(e){
         a.setAttribute("href", "#")
         a.innerText = "X"
         li.appendChild(a)
-        list.appendChild(li)       
+        list.appendChild(li)
+        newTask.value = ""
+        li.style.fontSize = "20px"
+        li.style.lineHeight = "1.5"
+        a.style.backgroundColor = "gray"       
+        a.style.color = "white"       
+        a.style.borderRadius = "10px"       
+        a.style.padding = "5px"       
+        a.style.fontSize = "15px"       
+        a.style.borderRadius = "2px"       
+        a.style.textDecoration = "none"       
     }
-    e.preventDefault()
+    // e.preventDefault()
 }
 function deleteTask(e){
     if(e.target.hasAttribute("href")){
@@ -28,4 +40,20 @@ function deleteTask(e){
             ele.remove()
         }
     }
+}
+function clearTask(){
+    if(confirm("Are you sure?")){
+        list.innerHTML = ""
+    }
+}
+function filterTask(e){
+    let text = e.target.value.toLowerCase()
+    document.querySelectorAll("li").forEach(task => {
+        let item = task.firstChild.textContent.toLowerCase().indexOf(text)
+        if (item != -1){
+            task.style.display = "block"
+        }else(
+            task.style.display = "none"
+        )
+    })
 }
